9adfc0ca51f3502ae8871e03dbf5cc38afe7bd96
zsmadani <madani.zaynabsm@gmail.com>
2025-09-20 11:25:09 +0330

    ocmod add - shamsi month
    ocmod added for report grouping by month to changed it to shamsi month

--- upload/admin/controller/extension/module/export_report.php
+++ upload/admin/controller/extension/module/export_report.php
@@ -1,7 +1,6 @@
 <?php
 require_once(DIR_SYSTEM . 'library/jdf.php');
-class ControllerExtensionModuleExportReport extends Controller
-{
+class ControllerExtensionModuleExportReport extends Controller{
     private $error = [];
 
     private function formatDateForCSV($date)
